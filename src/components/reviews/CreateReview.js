4444c57bb68e106dd6c9f2229dbfee3314e124d9
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-04-26 16:07:50 -0400

    Add User to Review

--- src/components/reviews/CreateReview.js
+++ src/components/reviews/CreateReview.js
@@ -7,19 +7,29 @@ const CreateReview = (props) => {
     const { user, restaurants, triggerRefresh} = props
     const [review, setReview] = useState(null)
 
+    addUser()
 
 
 
 
 
+    
 
+    const addUser = () => {
+        setReview(prevReview => {
+            const update = {'owner': user._id}
+            return {...prevReview, ...update}
+        })
+    }
 
-
-
-
-
-
-
+    return (
+        <ReviewForm
+            review={review}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            heading="Write A Review for This Restaurant"
+        />
+    )
 
 }
 
