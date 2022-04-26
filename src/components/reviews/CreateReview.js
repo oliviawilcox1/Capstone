b611dd89436e8e11cd64928939880cb92786448f
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-04-26 15:37:45 -0400

    ReviewForm Started and CrreateReview Started

---
+++ src/components/reviews/CreateReview.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import ReviewForm from '../shared/ReviewForm'
+import { createReview } from "../../api/reviews";
+
+
+const createReview = (props) => {
+    const { user, restaurants, triggerRefresh} = props
+    const [review, setReview] = useState(null)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/components/shared/ReviewForm.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import {Form, Container} from 'react-bootstrap'
+
+const ReviewForm = (props) => {
+    const { user, review, handleChange, handleSubmit, heading } = props
+    console.log('USER', user)
+
+    return (
+
+
+
+    )
+
+
+}
