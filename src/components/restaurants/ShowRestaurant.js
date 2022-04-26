2800fd76e256f3273c9e291c19f259950497181d
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-04-26 10:11:01 -0400

    Basic Format for the Show Page

--- src/components/restaurants/ShowRestaurant.js
+++ src/components/restaurants/ShowRestaurant.js
@@ -24,4 +24,43 @@ const ShowRestaurant = (props) => {
             })
             .catch(err => console.log(err))
     }, [])
+
+
+    if (!restaurant) {
+        return <p>Loading..</p>
+    }
+
+    return (
+        <>
+
+        <h1>{restaurant.name}</h1>
+        <img src={restaurant.image} alt=''/>
+        <p>Located at {restaurant.address}</p>
+        <p>{restaurant.visitors} visitors</p>
+        <p>{restaurant.cuisine}</p>
+        <p>{restaurant.rating}</p>
+        <div>
+            <h3>{restaurant.description}</h3>
+        </div>
+        <button>Add to your Future Eats</button>
+        <h3> Reviews </h3>
+        <button> Add a Review </button>
+        </>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
