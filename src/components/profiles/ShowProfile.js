b15afc8519f7db06ef1662491bac7bd59547fdef
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-04-27 21:26:59 -0400

    User Profile begun, and Favorite Cards Added

--- src/components/profiles/ShowProfile.js
+++ src/components/profiles/ShowProfile.js
@@ -13,11 +13,40 @@ const ShowProfile = (props) => {
             console.log(res.data.user)
         .catch(err => console.log(err))
     })
-    })
+})
+
+
+// going to need to map both the future eats and reviews
+
+// let futureEats
+// if (user.favorites.length > 0) {
+// futureEats = user.favorites.map(favorite => {
+//    return <div key={favorite.id} style={{width: '700px', textAlign:'center'}}>
+//         <Link to={`restaurants/${restaurant._id}`}><h2 style={{color: 'black', textDecorationLine: 'underline'}}>{favorite}</h2></Link>
+//     </div>
+// })
+// }
+
 
 return (
         <>
-      <p> {user.name}</p>
+        <div style={{width: '40%', textAlign:'left', display:'inline-block'}}>
+         <h1 style={{textAlign: 'left', margin: '20px'}}> User Profile </h1>
+        <img src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'alt="" style={{height: '300px', borderRadius: '35px', margin: '0 0 0 15px'}}/>
+        <h3 style={{margin: ' 25px'}}>Name: {user.name}</h3>
+        <h3 style={{margin: ' 25px'}}>Date Joined: {user.timestamps}</h3>
+        </div>
+        <div style={{ textAlign:'right', display:'inline-block'}}>
+        <h3 style={{textAlign: 'right'}}> Future Eats </h3>
+        <ul>
+            <li>
+                Hello
+                {/* {futureEats} */}
+            </li>
+        </ul>
+        <h3 style={{textAlign: 'right'}}> Previous Reviews </h3>
+        </div>
+
     </>
     )
 }
