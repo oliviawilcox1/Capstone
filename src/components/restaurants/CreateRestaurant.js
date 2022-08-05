9adfae856ff0454d43c0045c7a7c23c3b7f2a8ac
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-08-04 22:24:51 -0400

    Restaurant Form And Component Added

--- src/components/restaurants/CreateRestaurant.js
+++ src/components/restaurants/CreateRestaurant.js
@@ -1,23 +1,24 @@
 import React, {useState} from "react";
-import RestaurantForm from '../shared/ReviewForm'
+import RestaurantForm from '../shared/RestaurantForm'
 import { useParams, useNavigate} from 'react-router-dom'
 
 const CreateRestaurant = (props) => {
-    const { user, triggerRefresh, handleClose, msgAlert} = props
+    const { user, triggerRefresh, handleClose, restaurant, msgAlert} = props
+    const {id} = useParams()
 
     return (
         // pass props down and functions to review form component
-        // <RestaurantForm
-        //     restaurant={restaurant}
-        //     id={id}
-        //     user={user}
-        //     handleChange={handleChange}
-        //     handleSubmit={handleSubmit}
-        //     heading="Write Your Review"
-        // />
-        <>
-        <h1>Need to create a post for your website?</h1>
-        </>
+        <RestaurantForm
+            restaurant={restaurant}
+            id={id}
+            user={user}
+            // handleChange={handleChange}
+            // handleSubmit={handleSubmit}
+            heading="Write Your Review"
+        />
+        // <>
+        // <h1>Need to create a post for your website?</h1>
+        // </>
     )
 }
 
--- src/components/shared/RestaurantForm.js
+++ src/components/shared/RestaurantForm.js
@@ -4,30 +4,30 @@ import { useParams } from 'react-router-dom'
 
 const RestaurantForm = (props) => {
     // props received from create or edit review 
-    const { user, id, restaurant, handleChange, handleSubmit, heading } = props
+    const { user, id, restaurant, heading } = props
 
     return (
         <Container style={{ margin: 'auto'}}>
        
         <h1>{heading}</h1>
        {/* Submit Form  on clicking button */}
-        <Form onSubmit={handleSubmit}>
+        {/* <Form >
             <Form.Label></Form.Label>
             <Form.Control 
                     placeholder="Pick from 1 through 5"
                     value={review.rating}
                     name='rating'
-                    onChange={handleChange}
+            
             />
             <Form.Label >Description: </Form.Label>
             <Form.Control
                 placeholder="This was the most delicious meal I've had in ages! I am dreaming of coming back."
                 value={review.review}
                 name='review'
-                onChange={handleChange}
+            
             />
             <button type='submit'>Submit</button>  
-        </Form>
+        </Form> */}
         </Container>
     )
 }
